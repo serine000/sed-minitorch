9d1f9c6fa98942421750b157d2d703e624f01c40
Serine <serine_md@hotmail.com>
2026-09-16 07:12:34 +0300

    Implemented Module class methods and test suites for task0_4

--- minitorch/module.py
+++ minitorch/module.py
@@ -32,12 +32,14 @@ class Module:
 
     def train(self) -> None:
         "Set the mode of this module and all descendent modules to `train`."
+        self.training = True
         modules = self.modules()
         for module in modules:
             module.training = True
 
     def eval(self) -> None:
         "Set the mode of this module and all descendent modules to `eval`."
+        self.training = False
         modules = self.modules()
         for module in modules:
             module.training = False
@@ -72,8 +74,8 @@ class Module:
 
     def parameters(self) -> Sequence[Parameter]:
         "Enumerate over all the parameters of this module and its descendents."
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        parameters = self.named_parameters()
+        return [param[1] for param in parameters]
 
     def add_parameter(self, k: str, v: Any) -> Parameter:
         """
